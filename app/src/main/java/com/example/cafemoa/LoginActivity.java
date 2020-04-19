5f03f6a5703c70f85f7987c8410f6bda62d97b9e
gkdud4201 <gkdud4201@naver.com>
2020-04-20 00:17:55 +0900

    Initial commit

---
+++ app/src/main/java/com/example/cafemoa/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.example.cafemoa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button button =(Button)findViewById(R.id.signupbutton);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View view){
+                Intent intent=new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+
+}
